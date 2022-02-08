556f37ee5ca86ce9ef4bb29c9d58c4ea29364384
KooHyunJung <ghj3160@gmail.com>
2022-02-08 09:41:04 +0900

    프로그래머스 1레벨 1문제

--- algorithm36-40.py
+++ algorithm36-40.py
@@ -65,3 +65,17 @@ def solution(id_list, report, k):
 # report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 # k = 2
 # print(solution(id_list, report, k))
+
+
+# 프로그래머스 | 부족한 예산 찾기
+# https://programmers.co.kr/learn/courses/30/lessons/82612
+def solution(price, money, count):
+    answer = 0
+    for i in range(1,count+1):
+        answer += price * i
+    answer -= money
+    if answer < 0:
+        answer = 0
+    return answer
+
+# print(solution(3,20,4))
