184a389b756f1124644deaad16ec8fb027d89de0
KooHyunJung <ghj3160@gmail.com>
2022-02-17 18:08:34 +0900

    프로그램래머스 1레벨 1문제

--- algorithm41-45.py
+++ algorithm41-45.py
@@ -13,3 +13,17 @@ def solution(d, budget):
             count += 1
     return count
 #print(solution([2,2,3,3], 10))
+
+
+# 프로그래머스 | 없는 숫자 더하기
+# https://programmers.co.kr/learn/courses/30/lessons/86051
+def solution(numbers):
+    answer = []
+    for i in range(10):
+        if i not in numbers:
+            answer.append(i)
+    return sum(answer)
+# 다른 풀이   
+# def solution(numbers):
+#     return 45 - sum(numbers)
+# print(solution([1,2,3,4,6,7,8,0]))
