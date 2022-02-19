748e7637233536c1ebc580ed47df81c0d0a463a9
KooHyunJung <ghj3160@gmail.com>
2022-02-19 21:54:50 +0900

    프로그래머스 1레벨 1문제

--- algorithm41-45.py
+++ algorithm41-45.py
@@ -27,3 +27,14 @@ def solution(numbers):
 # def solution(numbers):
 #     return 45 - sum(numbers)
 # print(solution([1,2,3,4,6,7,8,0]))
+
+
+# 프로그래머스 | 문저욜 내 마음대로 정하기
+# https://programmers.co.kr/learn/courses/30/lessons/12915
+def solution(strings, n):
+    a = []
+    for i in strings:
+        a.append(i[n]+i)
+    answer = dict(zip(strings,a))
+    return list(dict(sorted(answer.items(), key=lambda x: x[1])).keys())
+#print(solution(["abce", "abcd", "cdx"], 2))
