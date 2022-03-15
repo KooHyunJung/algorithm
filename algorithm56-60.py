ec36882fe8cade10590bf5152e0ee997cc33db79
KooHyunJung <ghj3160@gmail.com>
2022-03-15 20:42:56 +0900

    프로그래머스 2레벨 1문제

--- algorithm56-60.py
+++ algorithm56-60.py
@@ -17,3 +17,30 @@ def solution(board: list, moves: list) -> int:
                 break
     return answer
 # print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
+
+
+# 프로그래머스 | 전화번호 목록
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+# 왜 아래는 안되는 걸까?
+def solution(phone_book):
+    SortedPhone = sorted(phone_book, key= len)
+    answer = True
+    for i in SortedPhone[1:]:
+        if SortedPhone[0] == i[:len(SortedPhone[0])]:
+            answer = False 
+            break
+    return answer
+# 해쉬 정석 !
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
+# print(solution(["12","123","1235","567","88"]))
