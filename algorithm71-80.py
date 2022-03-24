43d2d4052319155a80d010d4c4defc085535aabf
KooHyunJung <ghj3160@gmail.com>
2022-03-24 10:25:00 +0900

    백준 3문제

--- algorithm71-80.py
+++ algorithm71-80.py
@@ -7,3 +7,48 @@ def baekjoon(num):
     NumMax = num[-1]
     return NumMin * NumMax
 # print(baekjoon([3,4,2,12,6,8]))
+
+
+# 백준 | 최대공약수와 최소공배수
+# https://www.acmicpc.net/problem/2609
+# 최대공약수 math.gcd(a, b)
+# 최소공배수 math.lcm(a, b)
+import math
+def baekjoon(num):
+    greatestCD = math.gcd(num[0], num[1])
+    LeastCM = math.lcm(num[0], num[1])
+    return [greatestCD, LeastCM]
+# print(baekjoon([24, 18]))
+
+
+# 백준 | ACM 호텔
+# https://www.acmicpc.net/problem/10250
+def baekjoon(array):
+    H, _, N = array
+    if not N % H:
+        floor = H
+        num = N // H
+    else:
+        floor = N % H
+        num = N // H + 1
+    return floor*100+num
+# print(baekjoon([6, 12, 10]))
+
+
+# 백준 | 소수구하기
+# https://www.acmicpc.net/problem/1929
+# 다른거는 함수로 풀다가 이거는 그냥.. 그대로..
+# 에라토스테네스의 체
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+ 
+M, N = map(int, input().split())
+for i in range(M, N + 1):
+    if isPrime(i):
+        print(i)
