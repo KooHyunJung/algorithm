dbd35ecae27d1d33c67dea9de30b345093eb9ef8
KooHyunJung <ghj3160@gmail.com>
2022-06-12 13:42:08 +0900

    프로그래머스 1레벨 1문제 JS

--- algorithmJS01-10.js
+++ algorithmJS01-10.js
@@ -7,3 +7,21 @@ function solution(a, b) {
     return answer;
 }
 // console.log(solution(3,5));
+
+
+// 프로그래머스 | 자연수 뒤집어 배열
+// https://programmers.co.kr/learn/courses/30/lessons/12932
+// 변수.toString(); - 숫자를 문자열로 변환
+// 배열.reverse(); - 배열 뒤집기
+// 배열.map(x=>+x); - 배열의 모든 항목을 숫자로 변환
+function solution1(n) {
+    let re_num = n.toString();
+    re_num = Array.from(re_num).reverse().map(x=>+x);
+    return re_num
+}
+
+console.log(solution1(12345));
+
+
+// 프로그래머스 | 가운데 수 가져오기
+// https://programmers.co.kr/learn/courses/30/lessons/12903
