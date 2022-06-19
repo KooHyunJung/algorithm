7c73f2cd4d08e70e5079ef6906b588d00de1a71e
KooHyunJung <ghj3160@gmail.com>
2022-06-19 13:11:45 +0900

    update 프로그래머스 1레벨 1문제 JS

--- algorithmJS01-10.js
+++ algorithmJS01-10.js
@@ -154,7 +154,7 @@ function solution8(a, b) {
 // indexOf : https://hianna.tistory.com/379
 // splice : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 function solution9(arr) {
-    arr.splice(arr.indexOf(Math.min(...(arr), 1)));
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
     return arr.length ? arr : [-1];
 } 
 // console.log(solution9([4,3,2,1]))
