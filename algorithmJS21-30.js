f08bc953691493fd7654d5377c259af8b9fe882d
KooHyunJung <ghj3160@gmail.com>
2022-07-03 19:16:11 +0900

    프로그래머스 1레벨 1문제 JS

--- algorithmJS21-30.js
+++ algorithmJS21-30.js
@@ -15,7 +15,15 @@ function solution(array, commands) {
         return array.slice(v[0] - 1, v[1]).sort((a, b) => a - b).slice(v[2] - 1, v[2])[0];
     });
 }
-console.log(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
+// console.log(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
 
 
 
+// 프로그래머스 | 문자열을 정수로 바꾸기
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+// Number() :  인자로 전달된 문자열을 Number 변환. 문자나 undefined 등 인자로 전달하면 NaN(Not A Number) 반환.
+// parseInt() : 인자로 전달된 문자열을 Number 변환. 실수도 정수로 변환함.
+function solution1(s) {
+    return Number(s);
+}
+console.log(solution1("1234"))
