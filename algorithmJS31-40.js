c6c8984994f941ec1266779fe881f2b82aa73466
KooHyunJung <ghj3160@gmail.com>
2022-07-17 23:08:22 +0900

    프로그래머스 1레벨 1문제 JS

--- algorithmJS31-40.js
+++ algorithmJS31-40.js
@@ -6,3 +6,13 @@ function solution(price, money, count) {
     return total > 0 ? total : 0;
 }
 console.log(solution(3,20,4))
+
+
+
+// 프로그래머스 | 완주하지 못한 선수
+// https://programmers.co.kr/learn/courses/30/lessons/42576
+function solution1(participant, completion) {
+    var answer = '';
+    return answer;
+}
+console.log(solution1(["leo", "kiki", "eden"], ["eden", "kiki"]))
