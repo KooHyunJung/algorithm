467ccaeedb055412813110d6c2bf179ef1ff8470
KooHyunJung <ghj3160@gmail.com>
2022-07-18 20:40:04 +0900

    update 프로그래머스 1레벨 1문제

--- algorithmJS31-40.js
+++ algorithmJS31-40.js
@@ -12,7 +12,10 @@ console.log(solution(3,20,4))
 // 프로그래머스 | 완주하지 못한 선수
 // https://programmers.co.kr/learn/courses/30/lessons/42576
 function solution1(participant, completion) {
-    var answer = '';
-    return answer;
+    participant.sort();
+    completion.sort();
+    for(let i in participant) {
+        if(participant[i] !== completion[i]) return participant[i];
+    }
 }
 console.log(solution1(["leo", "kiki", "eden"], ["eden", "kiki"]))
