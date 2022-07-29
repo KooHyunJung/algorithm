067a5009c24b4b8f24e4c95b4fd35cb239b9559c
KooHyunJung <ghj3160@gmail.com>
2022-07-29 20:39:21 +0900

    프로그래머스 2레벨 1문제 JS

--- algorithmJS31-40.js
+++ algorithmJS31-40.js
@@ -90,10 +90,42 @@ function solution6(strings, n) {
     // n 번째 문자열 비교
     return strings.sort((s1, s2) => s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]));
 }
-
 console.log(solution6(["sun", "bed", "car"], 1))
 
 
 
 // 프로그래머스 | 신고 결과 받기
 // https://programmers.co.kr/learn/courses/30/lessons/92334
+function solution7(id_list, report, k) {
+    const newRepo = [...new Set(report)]; // report 중복 제거
+    const out = []; // 정지 아이디
+    const count = Array(id_list.length).fill(0); // 신고수 [0,0,0,0]
+    const mail = Array(id_list.length).fill(0); // 메일수 [0,0,0,0]
+
+    for (let r of newRepo) {
+        let a = r.split(" ")[0]; // 유저 ID
+        let b = r.split(" ")[1]; // 유저가 신고한 ID
+        let idxB = id_list.indexOf(b); // id_list에서 신고받은 유저의 index 칮기
+        count[idxB] += 1; // 신고받은 유저의 신고수 누적 더하기
+
+        // 신고수가 k개 이상인 경우
+        if (count[idxB] >= k) {
+        // 정지 리스트에 해당 아아디 추가
+        out.push(id_list[idxB])
+        }
+    }
+    newRepo.map((r, idx) => {
+        let a = r.split(' ')[0] // 유저 ID
+        let b = r.split(' ')[1] // 유저가 신고한 ID
+        // 신고한 ID(b)에 정지된 ID가 있는지 확인
+        if (out.indexOf(b) >= 0) {
+        // 신고한 ID 중에 정지된 ID가 있다면, 
+        // 해당 유저(a)의 id_list에서의 인덱스를 구하고
+        // mail의 해당 인덱스에 1 더하기
+            let idx = id_list.indexOf(a);
+            mail[idx] += 1;
+        } 
+    }) 
+    return mail
+}
+console.log(solution7(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
