3d0d3416c1e92d0d8ba6062be0c92bc4a344a60b
KooHyunJung <ghj3160@gmail.com>
2023-02-14 09:07:34 +0900

    프로그래머스 1레벨 1문제 py

--- algorithm90-100.py
+++ algorithm90-100.py
@@ -22,3 +22,28 @@ def solution1(s, skip, index):
     return answer
 
 print(solution1("aukks", "wbqd", 5))
+
+
+# 프로그래머스 | 개인정보 수집 유효기간
+# https://school.programmers.co.kr/learn/courses/30/lessons/150370
+"""
+문제 이해 
+return 유효기간이 지난 정보
+
+수도코드
+terms_dict = {약관 종류 : 총일수(오늘날짜-유효기간)}
+if 수집일자 <= terms_dict: 출력
+"""
+def day(today):
+    return int(today[0:4])*12*28 + int(today[5:7])*28 + int(today[8:])
+
+def solution2(today, terms, privacies):
+    terms_dict = {term[0]: day(today) - int(term[2:])*28 for term in terms}
+
+    answer = []
+    for num, privacie in enumerate(privacies):
+        if  day(privacie[:10]) <= terms_dict[privacie[-1:]]:
+            answer.append(num+1)
+    return answer
+
+print(solution2("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
